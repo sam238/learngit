c990346a74251bc5cba392ad1f34e9b6ec1ce694
sam <gongweishen@126.com>
2018-01-17 14:34:49 +0800

    增加def.py文件

---
+++ def.py
@@ -0,0 +1,15 @@
+from tkinter import *
+class Application(Frame):
+	def __init__(self,master=None):
+		Frame.__init__(self,master)
+		self.pack()
+		self.createWidgets()
+
+	def createWidgets(self):
+		self.helloLabel = Label(self,text='hello world')
+		self.helloLabel.pack()
+		self.quitButton = Button(self,text='Quit',command=self.quit)
+		self.quitButton.pack()
+app = Application()
+app.master.title('hello world')
+app.mainloop()
